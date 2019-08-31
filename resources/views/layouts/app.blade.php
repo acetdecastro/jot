90b87ee3846a33d7676a40fd8094cef4823ec055
acetdecastro <acetdecastro@gmail.com>
2019-08-31 14:13:28 +0800

    :star: Auth View Design - Finish Log In, Register, Forgot pw design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -70,9 +70,9 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> --}}
 
-        <main class="py-4">
+        <main class="h-screen">
             @yield('content')
         </main>
     </div>
